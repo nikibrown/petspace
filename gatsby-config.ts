cd780d9ad63809e67dbf348db3f325ca2f555765
Niki Brown <hi@nikibrown.com>
2023-06-27 19:18:40 -0700

    setup, gatsby config

---
+++ gatsby-config.ts
@@ -0,0 +1,39 @@
+import type { GatsbyConfig } from "gatsby"
+
+require("dotenv").config({
+	path: `.env.${process.env.NODE_ENV}`,
+})
+
+
+const config: GatsbyConfig = {
+  siteMetadata: {
+    title: `PetSpace`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
+  // If you use VSCode you can also use the GraphQL plugin
+  // Learn more at: https://gatsby.dev/graphql-typegen
+  graphqlTypegen: true,
+  plugins: [{
+    resolve: 'gatsby-source-contentful',
+    options: {
+      spaceId: process.env.CONTENTFUL_SPACE_ID,
+      accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+      host: process.env.CONTENTFUL_HOST,
+    },
+  }, "gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp", "gatsby-plugin-styled-components", "gatsby-plugin-sitemap", {
+    resolve: 'gatsby-plugin-manifest',
+    options: {
+      "icon": "src/images/icon.png"
+    }
+  }, {
+    resolve: 'gatsby-source-filesystem',
+    options: {
+      "name": "images",
+      "path": "./src/images/"
+    },
+    __key: "images"
+  }]
+};
+
+export default config;
